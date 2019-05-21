dce1859b44108ade075f045d99c807fad13c5fe0
Jackmc <jackmc1122@gmail.com>
2019-05-20 17:01:35 -0700

    Add header through index.js and remove the static one.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import todos from '../data/todos.js';
 import htmlToDOM from './html-to-DOM.js';
 import todoTemplate from './todo-template.js';
+import headerTemplate from './header-template.js';
 
 const todosEl = document.getElementById('todos');
+
+const header = headerTemplate();
+const headerDOM = htmlToDOM(header);
+document.body.firstChild.before(headerDOM);
 
 todos.forEach(todo => {
   const html = todoTemplate(todo);
