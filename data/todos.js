a0260eb227da1403e5549d00a68c787dbac1bc1e
Jackmc <jackmc1122@gmail.com>
2019-05-20 17:14:41 -0700

    Add some more data.

--- data/todos.js
+++ data/todos.js
@@ -6,7 +6,27 @@ const todos = [
   {
     task: 'Learn forEach',
     completed: false
-  }
+  },
+  {
+    task: 'Learn map',
+    completed: false
+  },
+  {
+    task: 'Learn reduce',
+    completed: false
+  },
+  {
+    task: 'Learn filter',
+    completed: false
+  },
+  {
+    task: 'Learn for...of',
+    completed: false
+  },
+  {
+    task: 'Learn for...in',
+    completed: false
+  },
 ];
 
 export default todos;
